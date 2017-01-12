3f7ea9e6fc8deb74203f398ef3e4d22054096415
Mark Lambert <marklam@spunow.co.uk>
2017-01-12 10:52:50 +0000

    Example test

---
+++ UsingFsCheckWithNUnit3TestAdapter/Fixture.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace UsingFsCheckWithNUnitTestAdapter
+{
+    [TestFixture]
+    public class Fixture
+    {
+        [FsCheck.NUnit.Property]
+        public bool MultiplyingByZeroIsZero(int x)
+        {
+            return (x * 0) == 0;
+        }
+    }
+}
